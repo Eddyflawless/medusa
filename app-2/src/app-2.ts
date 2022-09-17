9851ae0bced806fab11e3e9798ed84a2067e2530
darkvadr <edjonorh@gmail.com>
2022-09-17 10:34:29 +0000

    added time function

--- app-2/src/app-2.ts
+++ app-2/src/app-2.ts
@@ -7,4 +7,8 @@ function apiCli(){
     console.log(hello("This world is awesome2. Cuz i can"))
 }
 
+function getTime() {
+    return new Date().getTime();
+}
+console.log("getTime", getTime)
 apiCli()
